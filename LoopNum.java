f00b79384da595ac73afcf2ed12bf830f9c9eece
onnipa17 <152240702+onnipa17@users.noreply.github.com>
2024-07-31 15:44:09 +0700

    Create LoopNum.java

---
+++ LoopNum.java
@@ -0,0 +1,8 @@
+public class LoopNum {
+    public static void main(String[] args) {
+        for(int number = 1; number < 5; number++){
+            System.err.println(number+" ");
+        }
+
+    }
+}
